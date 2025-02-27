09c39d25c0598e5b515f48513b1af4abba44c351
Sunny Tseng <tzuchientseng@gmail.com>
2025-02-27 23:07:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/intro">簡介</RouterLink>
+        <RouterLink to="/elementary">國小課綱</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
